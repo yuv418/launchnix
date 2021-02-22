50292aaf21faeeb4a5da3d391595e229ca024092
Ramesh Balaji <post2rb@hotmail.com>
2021-02-21 20:30:17 -0500

    Change install script for env var

--- default.nix
+++ default.nix
@@ -29,6 +29,9 @@ let
                 pkgs.libvirt
             ];
             installPhase = drv.installPhase + ''
+                mv $bin/bin/launchnix $bin/bin/launchnix-bin
+                echo "#!/bin/sh" >> $bin/bin/launchnix
+                echo MORPH_PATH=${pkgs.morph}/bin/morph $bin/bin/launchnix-bin >> $bin/bin/launchnix
                 cp -r $src/nix $bin/bin/
             '';
           };
