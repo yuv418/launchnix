1b19728ba8ea33eeed297a792c4b6f45c5e63d6c
Ramesh Balaji <post2rb@hotmail.com>
2021-04-16 11:59:51 -0400

    Slightly better/(worse?) qcow2 error handling

--- src/qcow2.rs
+++ src/qcow2.rs
@@ -5,16 +5,22 @@ use std::process::Command;
 
 pub fn create(output_path: &PathBuf, size: u64) -> Result<(), String> {
     // Size is in bytes
+
+    let lazy_error_message = "Failed to create the blank image for your non-NixOS VM.";
     let img_handle = Command::new("qemu-img")
         .arg("create")
-        .arg("-f")
+        .arg("-g")
         .arg("qcow2")
         .arg(output_path.to_str().unwrap())
         .arg(size.to_string() + "M")
         .status()
-        .map_err(|e| "Failed to create the blank image for your non-NixOS VM.")?;
+        .map_err(|e| lazy_error_message)?;
 
-    Ok(())
+    if img_handle.success() {
+        Ok(())
+    } else {
+        Err(lazy_error_message.to_owned())
+    }
 }
 
 fn dd(input_path: &str, output_path: &str) -> Result<(), String> {
